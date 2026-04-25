7e30d88b728a4842e06bc2f0e247a7154016237f
achraf youssef <yachraf403@gmail.com>
2026-04-25 22:45:03 +0100

    feat: Update API base URL and network security configuration; add new domain for secure traffic

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        buildConfigField("String", "API_BASE_URL", "\"http://10.0.2.2:8080/\"")
+        buildConfigField("String", "API_BASE_URL", "\"http://192.168.0.218:8080/\"")
     }
 
     buildTypes {
